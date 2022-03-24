7824832a4607e9927b193e119f7df57e45c9af1e
kathy <kathy_li@brown.edu>
2022-03-24 17:50:57 -0400

    done

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Horoscope from './Horoscope';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Horoscope/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
